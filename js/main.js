7a1c6b920671f58d127706c1d12fc71d1a0231af
Marcela B Petroli <marcelabpetroli@gmail.com>
2022-11-15 11:18:24 +0100

    Add computerWin and playerWin functions + counters

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-"use strict";
+'use strict';
 
-const btnBattleElement = document.querySelector(".js-btn-battle");
-const playerChoiceElement = document.querySelector(".js-select");
-const numberOfBattlesElement = document.querySelector(".js-number-battles");
+const btnBattleElement = document.querySelector('.js-btn-battle');
+const playerChoiceElement = document.querySelector('.js-select');
+const numberOfBattlesElement = document.querySelector('.js-number-battles');
 
-let playerScore = document.querySelector(".player-score");
-let computerScore = document.querySelector(".computer-score");
+let playerScore = document.querySelector('.player-score');
+let computerScore = document.querySelector('.computer-score');
 
-const message = document.querySelector(".js-start-battle");
-const btnRestartGameElement = document.querySelector(".js-btn-restart");
+const message = document.querySelector('.js-start-battle');
+const btnRestartGameElement = document.querySelector('.js-btn-restart');
 
 let numberBattles = 0;
+let pScore = 0;
+let cScore = 0;
 
 //Computer can choose one of these options
 const computerOptions = [2, 2, 2, 3, 5];
@@ -30,27 +32,38 @@ function handleClick(e) {
 function playGame() {
   const computerChoice = computerOptions[getRandomNumber(5) - 1];
   const playerChoice = parseInt(playerChoiceElement.value);
+
   if (playerChoice === computerChoice) {
     message.textContent = `Empate 💥`;
     numberBattles++;
     numberOfBattlesElement.textContent = numberBattles;
   } else if (playerChoice > computerChoice) {
-    message.textContent = `Ha ganado el Ejército del Bien! Enhorabuena ⚔️ 🛡`;
-    playerScore.textContent = parseInt(playerScore.textContent) + 1;
-    numberBattles++;
-    numberOfBattlesElement.textContent = numberBattles;
+    playerWins();
   } else {
     message.textContent = `Ha ganado el Ejército del Mal! Vuelve a intentarlo 👾 ❌`;
-    computerScore.textContent = parseInt(computerScore.textContent) + 1;
-    numberBattles++;
-    numberOfBattlesElement.textContent = numberBattles;
+    computerWins();
   }
-
-  if (parseInt(numberOfBattlesElement.textContent) === 10) {
-    btnRestartGameElement.classList.remove("hidden");
-    btnBattleElement.classList.add("hidden");
+  if (parseInt(numberBattles) === 10) {
+    btnRestartGameElement.classList.remove('hidden');
+    btnBattleElement.classList.add('hidden');
   }
 }
 
+function computerWins() {
+  message.textContent = `Ha ganado el Ejército del Mal! Vuelve a intentarlo 👾 ❌`;
+  cScore++;
+  numberBattles++;
+  computerScore.innerHTML = cScore;
+  numberOfBattlesElement.textContent = numberBattles;
+}
+
+function playerWins() {
+  message.textContent = `Ha ganado el Ejército del Bien! Enhorabuena ⚔️ 🛡`;
+  pScore++;
+  numberBattles++;
+  playerScore.innerHTML = pScore;
+  numberOfBattlesElement.textContent = numberBattles;
+}
+
 //Listening to event click on the buttn "BATALLA"
-btnBattleElement.addEventListener("click", handleClick);
+btnBattleElement.addEventListener('click', handleClick);
